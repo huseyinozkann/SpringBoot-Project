957fee68ad08830545857153fb847673dbd5e31f
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-20 22:20:13 +0300

    Component Scanning added

---
+++ 2-Spring-Boot-Spring-Core/2-component-scanning/src/main/java/com/huseyinozkan/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.huseyinozkan.springcoredemo.rest;
+
+import com.huseyinozkan.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}

809031a8a092a4f638d79ed2c9ce5c3992a466c2
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-30 17:16:46 +0300

    Lazy Initialization added

---
+++ 2-Spring-Boot-Spring-Core/06-lazy-initialization/src/test/java/com/huseyinozkan/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huseyinozkan.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

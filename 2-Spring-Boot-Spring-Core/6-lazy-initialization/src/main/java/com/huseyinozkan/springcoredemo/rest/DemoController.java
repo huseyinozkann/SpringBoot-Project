58a30472f7d6a0cd1f4fea6e3243814dbbb25489
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-06-30 17:25:03 +0300

    Lazy Initialization completed

---
+++ 2-Spring-Boot-Spring-Core/6-lazy-initialization/src/main/java/com/huseyinozkan/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.huseyinozkan.springcoredemo.rest;
+
+import com.huseyinozkan.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach myCoach) {
+
+        System.out.println("In constructor : " + getClass().getSimpleName());
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}

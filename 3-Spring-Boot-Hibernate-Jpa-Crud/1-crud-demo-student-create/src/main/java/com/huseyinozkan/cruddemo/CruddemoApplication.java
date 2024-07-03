43fa8e1284576eff214b075d84517949bbb74abd
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-07-04 01:15:17 +0300

    Crud Demo Create Function added

--- 3-Spring-Boot-Hibernate-Jpa-Crud/1-crud-demo-student-create/src/main/java/com/huseyinozkan/cruddemo/CruddemoApplication.java
+++ 3-Spring-Boot-Hibernate-Jpa-Crud/1-crud-demo-student-create/src/main/java/com/huseyinozkan/cruddemo/CruddemoApplication.java
@@ -1,13 +1,55 @@
 package com.huseyinozkan.cruddemo;
 
+import com.huseyinozkan.cruddemo.dao.StudentDAO;
+import com.huseyinozkan.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		return runner ->{
+			createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
+		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("Ali", "Veli", "ali@veli.com");
+		Student tempStudent2 = new Student("Hasan", "Hüseyin", "hasan@huseyin.com");
+		Student tempStudent3 = new Student("Mehmet", "Mustafa", "mehmet@mustafa.com");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Hüseyin", "Özkan", "huseyinozkan.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }

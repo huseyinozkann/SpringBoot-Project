f4b6739d4ced337bccd4ae9105018d7ae986c7ac
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-07-04 18:37:05 +0300

    Crud Demo Student Query FindByLastName added

---
+++ 3-Spring-Boot-Hibernate-Jpa-Crud/4-crud-demo-student-query-findByLastName/src/main/java/com/huseyinozkan/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,17 @@
+package com.huseyinozkan.cruddemo.dao;
+
+import com.huseyinozkan.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+}

3d8675eef3b8fa73b7cca9d2f96d9cf2ae3d9fbf
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-07-10 11:27:17 +0300

    Rest-Crud-Students-List-Refactored added

---
+++ 4-Spring-Boot-Rest-Crud/3-spring-boot-rest-crud-students-list-refactore/src/main/java/com/huseyinozkan/demo/rest/StudentRestController.java
@@ -0,0 +1,37 @@
+package com.huseyinozkan.demo.rest;
+
+import com.huseyinozkan.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData(){
+
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Huseyin", "Ozkan"));
+        theStudents.add(new Student("Ali", "Veli"));
+        theStudents.add(new Student("Hasan", "Ozkan"));
+    }
+
+
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return theStudents;
+    }
+}

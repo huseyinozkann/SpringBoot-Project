7c3b6cd58d65bd0ffa17682977edeb51b7a58897
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-12 00:26:21 +0300

    Rest-Crud-Employee-Dao-Find-Add-Update-Delete added

---
+++ 4-Spring-Boot-Rest-Crud/9-spring-boot-rest-crud-employee-dao-find-add-update-delete/src/main/java/com/huseyinozkan/demo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.huseyinozkan.demo.service;
+
+import com.huseyinozkan.demo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}

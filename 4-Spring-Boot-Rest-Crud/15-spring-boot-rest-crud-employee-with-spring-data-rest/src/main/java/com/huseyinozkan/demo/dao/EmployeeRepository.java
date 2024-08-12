da2a77f58119f4aacfea5da778e3d34cc56770c5
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-12 12:26:33 +0300

    Rest-Crud-Employee-with-Spring-Data-Rest-Configs-and-Sorting added

--- 4-Spring-Boot-Rest-Crud/15-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/huseyinozkan/demo/dao/EmployeeRepository.java
+++ 4-Spring-Boot-Rest-Crud/15-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/huseyinozkan/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.huseyinozkan.demo.dao;
 
 import com.huseyinozkan.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }

2eeb823765458fae6378ff8ddd2110dd0b3d682e
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-24 20:51:57 +0300

    Form Data Binding Text Fields

---
+++ 6-spring-boot-spring-mvc/07-form-data-binding-textfields/src/main/java/com/huseyinozkan/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.huseyinozkan.thymeleafdemo.controller;
+
+import com.huseyinozkan.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel){
+
+       //create student
+        Student theStudent = new Student();
+
+        // add student object to the model
+        theModel.addAttribute("student", theStudent);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+}

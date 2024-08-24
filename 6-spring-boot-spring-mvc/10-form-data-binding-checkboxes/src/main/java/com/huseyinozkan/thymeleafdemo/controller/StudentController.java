60378fbc702306ea5526fd785990a7919d817893
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-25 01:13:42 +0300

    Form Data Binding Checkboxes Read from Props File

--- 6-spring-boot-spring-mvc/10-form-data-binding-checkboxes/src/main/java/com/huseyinozkan/thymeleafdemo/controller/StudentController.java
+++ 6-spring-boot-spring-mvc/10-form-data-binding-checkboxes/src/main/java/com/huseyinozkan/thymeleafdemo/controller/StudentController.java
@@ -19,6 +19,9 @@ public class StudentController {
     @Value("${languages}")
     private List<String> languages;
 
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
@@ -33,6 +36,9 @@ public class StudentController {
 
         //add languages object to the model
         theModel.addAttribute("languages", languages);
+
+        // add the list of systems to the model
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
     }

ece030069445ee7160debcc1e82ae1c050ba6a6c
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-25 00:09:35 +0300

    Dropdown List Read from Props File

--- 6-spring-boot-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/huseyinozkan/thymeleafdemo/controller/StudentController.java
+++ 6-spring-boot-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/huseyinozkan/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.huseyinozkan.thymeleafdemo.controller;
 
 import com.huseyinozkan.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -18,6 +24,9 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        //add countries object to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }

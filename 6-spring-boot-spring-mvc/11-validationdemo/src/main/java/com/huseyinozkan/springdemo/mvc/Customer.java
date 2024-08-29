be58cba4d7aa73eb1a9ecca4ab720eb88c9576cc
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-29 23:44:59 +0300

    Validation Demo - Number Ranges

--- 6-spring-boot-spring-mvc/11-validationdemo/src/main/java/com/huseyinozkan/springdemo/mvc/Customer.java
+++ 6-spring-boot-spring-mvc/11-validationdemo/src/main/java/com/huseyinozkan/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.huseyinozkan.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,20 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1, message="is required")
     private String lastName = "";
+
+    @Min(value=0, message="must be greater than or equal to zero")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePasses;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(@Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;

c3e4697d6a2efae6c9ce384c9f925b3b5280a166
hüseyinözkan <ozkan.hsyn1@gmail.com>
2024-08-30 14:53:10 +0300

    Validation Demo - Make Integer Fields Required

--- 6-spring-boot-spring-mvc/11-validationdemo/src/main/java/com/huseyinozkan/springdemo/mvc/Customer.java
+++ 6-spring-boot-spring-mvc/11-validationdemo/src/main/java/com/huseyinozkan/springdemo/mvc/Customer.java
@@ -10,6 +10,7 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
+    @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
     private int freePasses;
